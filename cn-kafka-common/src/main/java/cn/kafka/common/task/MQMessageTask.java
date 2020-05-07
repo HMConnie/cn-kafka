6a76a154d1e74011613cee50cca4c165b5301991
hinge <kang.yang@shanggucai.com>
2020-05-07 10:24:57 +0800

    提交kafka示例

--- cn-kafka-common/src/main/java/cn/kafka/common/task/MQMessageTask.java
+++ cn-kafka-common/src/main/java/cn/kafka/common/task/MQMessageTask.java
@@ -11,6 +11,8 @@ import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
